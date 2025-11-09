7131b52e1f56bc426846efe017e6c83d54c76976
khanhtimn <khanhlcbb@gmail.com>
2025-11-09 11:42:14 +0700

    Porting to neoforge 1.21.1

--- src/main/java/dev/khanhtimn/gravitychanger/GravityMixinPlugin.java
+++ src/main/java/dev/khanhtimn/gravitychanger/GravityMixinPlugin.java
@@ -1,38 +1,15 @@
 package dev.khanhtimn.gravitychanger;
 
-import com.bawnorton.mixinsquared.MixinSquaredBootstrap;
-import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
-import me.fallenbreath.conditionalmixin.api.checker.RestrictionChecker;
-import me.fallenbreath.conditionalmixin.api.checker.RestrictionCheckers;
-import me.fallenbreath.conditionalmixin.api.mixin.AnnotationCleaner;
-import org.objectweb.asm.tree.ClassNode;
-import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
-import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+import me.fallenbreath.conditionalmixin.api.mixin.RestrictiveMixinConfigPlugin;
 
 import java.util.List;
 import java.util.Set;
 
-public class GravityMixinPlugin implements IMixinConfigPlugin {
-
-    protected final RestrictionChecker restrictionChecker = RestrictionCheckers.memorized();
-    private final AnnotationCleaner annotationCleaner = AnnotationCleaner.create(Restriction.class);
-
-    public GravityMixinPlugin() {
-        this.restrictionChecker.setFailureCallback(this::onRestrictionCheckFailed);
-    }
-
-    @Override
-    public void onLoad(String mixinPackage) {
-    }
+public class GravityMixinPlugin extends RestrictiveMixinConfigPlugin {
 
     @Override
     public String getRefMapperConfig() {
         return null;
-    }
-
-    @Override
-    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-        return this.restrictionChecker.checkRestriction(mixinClassName);
     }
 
     @Override
@@ -42,23 +19,6 @@ public class GravityMixinPlugin implements IMixinConfigPlugin {
 
     @Override
     public List<String> getMixins() {
-        return null;
-    }
-
-    @Override
-    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-        this.annotationCleaner.onPreApply(targetClass);
-    }
-
-    @Override
-    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-        this.annotationCleaner.onPostApply(targetClass);
-    }
-
-    /**
-     * go override it and do something you want, e.g. logging
-     */
-    protected void onRestrictionCheckFailed(String mixinClassName, String reason)
-    {
+        return List.of();
     }
 }
